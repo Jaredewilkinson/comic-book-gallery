fb8c1553cf9db22f371317a2c929d12f23f28fc1
Jared Wilkinson <jwilkinson@ustcorp.net>
2018-08-02 10:05:40 -0600

    Added design files

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -10,6 +10,17 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Super Mario 64";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Super Mario 64 is a 1996 platform video game developed and published by Nintendo for the Nintendo 64.<p>";
+            ViewBag.artists = new string[]
+            {
+        "Mario",
+        "Princess Peach",
+        "Bowser",
+        "Toad",
+        "Yoshi"
+            };
             //if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
             //{
 
